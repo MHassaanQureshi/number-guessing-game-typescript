c76e4ac73f6fcca440a45a2ef0ec40a1145e9af4
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-05-08 20:10:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10);
+const answer = await inquirer.prompt([
+    {
+        name: "userguess",
+        type: "number",
+        message: "please guess a number between 1 to 9: ",
+    },
+]);
+if (answer.userguess === randomnumber) {
+    console.log("you guessed it right");
+}
+else {
+    console.log("you guessed it wrong");
+}
